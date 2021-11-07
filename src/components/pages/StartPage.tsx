b0f63ad9fa9b3252785d401ecb27d9f0b0892bae
YankaZabka <yanka.zabka@gmail.com>
2021-11-07 17:04:24 +0300

    Code StartPage component

---
+++ src/components/pages/StartPage.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import classes from "../../scss/StartPage.module.scss"
+
+interface StartPageProps {
+
+}
+
+const StartPage: React.FC<StartPageProps> = () => {
+    return (
+        <div className={classes.startPage}>
+
+        </div>
+    );
+};
+
+export default StartPage;
