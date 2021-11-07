b63d7a47d3226f53a254a82fe97f3f88cd2f8b7a
YankaZabka <yanka.zabka@gmail.com>
2021-11-07 17:16:17 +0300

    Code CardTitle & CardOptions components

---
+++ src/components/CardTitle.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import classes from "../scss/CardTitle.module.scss"
+
+interface CardTitleProps {
+    text: string,
+}
+
+const CardTitle: React.FC<CardTitleProps> = ({text}) => {
+    return (
+        <h2 className={classes.title}>
+            {text}
+        </h2>
+    );
+};
+
+export default CardTitle;
