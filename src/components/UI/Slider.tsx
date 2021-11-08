7aa8091893ad393c3731ed655a0481ea474db26c
YankaZabka <yanka.zabka@gmail.com>
2021-11-08 17:59:56 +0300

    Delete comment from Slider

--- src/components/UI/Slider.tsx
+++ src/components/UI/Slider.tsx
@@ -6,16 +6,6 @@ interface SliderProps {
 }
 
 const Slider: React.FC<SliderProps> = () => {
-
-    // generateBackground() {
-    //     if (value === min) {
-    //         return
-    //     }
-    //
-    //     const percentage =  (value - min) / (max - min) * 100
-    //     return 'background: linear-gradient(to right, #f9ca24 ' + percentage + '%, #F7F9FF ' + percentage + '%)'
-    // }
-
     return (
         <div className={classes.sliderContainer}>
             <input
