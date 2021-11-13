47d12ca6beb43bf756cd5d64fe646b5dad2ab671
YankaZabka <yanka.zabka@gmail.com>
2021-11-13 14:45:54 +0300

    Add state to App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,41 @@
-import React from 'react';
+import React, {useState} from 'react';
 import classes from "./App.module.scss";
 import Header from "./components/Header";
 import StartPage from "./components/pages/StartPage";
 
 const App = () => {
-  return (
-    <div className={classes.App}>
-        <Header/>
-        <StartPage/>
-    </div>
-  );
+    const [numberOfWords, setNumberOfWords] = useState<number>(6)
+    const [startingDistance, setStartingDistance] = useState<number>(25)
+    const [lettersInWords, setLettersInWords] = useState<number>(8)
+    const [increasingDistance, setIncreasingDistance] = useState<number>(25)
+    const [speed, setSpeed] = useState<number>(1)
+
+    const changeSpeed = (value: number): void => {
+        setSpeed(value)
+    }
+
+    const changeIncreasingDistance = (value: number): void => {
+        setIncreasingDistance(value)
+    }
+
+    const changeLettersInWords = (value: number): void => {
+        setLettersInWords(value)
+    }
+
+    const changeStartingDistance = (value: number): void => {
+        setStartingDistance(value)
+    }
+
+    const changeNumberOfWords = (value: number): void => {
+        setNumberOfWords(value)
+    }
+
+    return (
+        <div className={classes.App}>
+            <Header/>
+            <StartPage/>
+        </div>
+    );
 }
 
 export default App;
