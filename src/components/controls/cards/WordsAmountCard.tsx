953f0104d6340683c420c608d84fa700bbb3a699
YankaZabka <yanka.zabka@gmail.com>
2021-11-13 15:12:53 +0300

    Add interaction between WordsAmountCard and CardOptions&Slider

--- src/components/controls/cards/WordsAmountCard.tsx
+++ src/components/controls/cards/WordsAmountCard.tsx
@@ -1,15 +1,24 @@
-import React from 'react';
+import React, {useState} from 'react';
 import SettingCardTemplate from "../SettingCardTemplate";
 import Slider from "../../generic/Slider";
 import CardTitle from "../CardTitle";
 import CardOptions from "../CardOptions";
 
 const WordsAmountCard = () => {
+    const [currentValue, setCurrentValue] = useState<number>(50)
+
+    const handleChange = (value: number): void => {
+        setCurrentValue(value)
+    }
+
     return (
         <SettingCardTemplate>
             <CardTitle text={"Сколько слов"}/>
-            <CardOptions/>
-            <Slider/>
+            <CardOptions
+                options={[1, 2, 3, 4, 5, 6, 7, 8, 9, 10]}
+                current={currentValue}
+            />
+            <Slider changeValue={handleChange}/>
         </SettingCardTemplate>
     );
 };
--- src/components/generic/Slider.tsx
+++ src/components/generic/Slider.tsx
@@ -1,14 +1,41 @@
-import React from 'react';
+import React, {useState} from 'react';
 import classes from "./Slider.module.scss";
 
-const Slider = () => {
+interface SliderProps {
+    changeValue(value: number): void
+}
+
+const Slider = ({changeValue}: SliderProps) => {
+    const [value, setValue] = useState<number>()
+    const [background, setBackground] = useState<string>("linear-gradient(to right, #f9ca24 50%, #F7F9FF 50%)")
+
+    const generateBackground = (value: number, min: number, max: number): void | undefined => {
+        if (value === min) {
+            return
+        }
+
+        const percentage = (value - min) / (max - min) * 100
+        setBackground('linear-gradient(to right, #f9ca24 ' + percentage + '%, #F7F9FF ' + percentage + '%)')
+    }
+
     return (
         <div className={classes.sliderContainer}>
             <input
                 type="range"
-                min={0}
+                min={1}
                 max={100}
                 className={classes.slider}
+                style={{background: background}}
+                onChange={(e) => {
+                    generateBackground(value!, 0, 100)
+                    setValue(Number(e.target.value))
+                    changeValue(value!)
+                }}
+                onClick={(e) => {
+                    generateBackground(value!, 0, 100)
+                    setValue(Number((e.target as HTMLInputElement).value))
+                    changeValue(value!)
+                }}
             />
         </div>
     );
