326e11544598cdaa62eb01950a07b5880dbcd62c
YankaZabka <yanka.zabka@gmail.com>
2021-11-16 18:41:34 +0300

    Move options state from App to StartPage

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,13 @@
-import React, {useState} from 'react';
+import React from 'react';
 import classes from "./App.module.scss";
 import Header from "./components/Header";
 import StartPage from "./components/pages/StartPage";
 
 const App = () => {
-    const [numberOfWords, setNumberOfWords] = useState<number>(6)
-    const [startingDistance, setStartingDistance] = useState<number>(25)
-    const [lettersInWords, setLettersInWords] = useState<number>(8)
-    const [increasingDistance, setIncreasingDistance] = useState<number>(25)
-    const [speed, setSpeed] = useState<number>(1)
-
-    const changeSpeed = (value: number): void => {
-        setSpeed(value)
-    }
-
-    const changeIncreasingDistance = (value: number): void => {
-        setIncreasingDistance(value)
-    }
-
-    const changeLettersInWords = (value: number): void => {
-        setLettersInWords(value)
-    }
-
-    const changeStartingDistance = (value: number): void => {
-        setStartingDistance(value)
-    }
-
-    const changeNumberOfWords = (value: number): void => {
-        setNumberOfWords(value)
-    }
-
     return (
         <div className={classes.App}>
             <Header/>
-            <StartPage
-                handleSpeed={changeSpeed}
-                handleIncreasingDistance={changeIncreasingDistance}
-                handleLettersInWords={changeLettersInWords}
-                handleStartingDistance={changeStartingDistance}
-                handleNumberOfWords={changeNumberOfWords}
-            />
+            <StartPage/>
         </div>
     );
 }
