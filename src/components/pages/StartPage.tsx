ab7b13d909ad1b2840e99376048630fa6bb58ec6
YankaZabka <yanka.zabka@gmail.com>
2021-11-16 18:41:34 +0300

    Move options state from App to StartPage

--- src/components/pages/StartPage.tsx
+++ src/components/pages/StartPage.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import classes from "./StartPage.module.scss"
 import WordsAmountCard from "../controls/WordsAmountCard";
 import SpeedCard from "../controls/SpeedCard";
@@ -7,27 +7,20 @@ import StartDistanceCard from "../controls/StartDistanceCard";
 import LettersAmountCard from "../controls/LettersAmountCard";
 import IncreasingDistanceCard from "../controls/IncreasingDistanceCard";
 
-interface StartPageProps {
+const StartPage = () => {
+    const [numberOfWords, setNumberOfWords] = useState<number>(6)
+    const [startingDistance, setStartingDistance] = useState<number>(25)
+    const [lettersInWords, setLettersInWords] = useState<number>(8)
+    const [increasingDistance, setIncreasingDistance] = useState<number>(25)
+    const [speed, setSpeed] = useState<number>(1)
 
-    handleSpeed(value: number): void
-
-    handleIncreasingDistance(value: number): void
-
-    handleLettersInWords(value: number): void
-
-    handleStartingDistance(value: number): void
-
-    handleNumberOfWords(value: number): void
-}
-
-const StartPage = ({handleSpeed, handleIncreasingDistance, handleNumberOfWords, handleLettersInWords, handleStartingDistance}: StartPageProps) => {
     return (
         <div className={classes.startPage}>
-            <WordsAmountCard handleNumberOfWords={handleNumberOfWords}/>
-            <StartDistanceCard handleStartingDistance={handleStartingDistance}/>
-            <LettersAmountCard handleLettersInWords={handleLettersInWords}/>
-            <IncreasingDistanceCard handleIncreasingDistance={handleIncreasingDistance}/>
-            <SpeedCard handleSpeed={handleSpeed}/>
+            <WordsAmountCard handleNumberOfWords={value => setNumberOfWords(value)}/>
+            <StartDistanceCard handleStartingDistance={value => setStartingDistance(value)}/>
+            <LettersAmountCard handleLettersInWords={value => setLettersInWords(value)}/>
+            <IncreasingDistanceCard handleIncreasingDistance={value => setIncreasingDistance(value)}/>
+            <SpeedCard handleSpeed={value => setSpeed(value)}/>
             <StartButton/>
         </div>
     );
