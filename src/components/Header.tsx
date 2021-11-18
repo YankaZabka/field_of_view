779eaf48023d4607f4711a4bc6709cc9c350db73
YankaZabka <yanka.zabka@gmail.com>
2021-11-18 15:40:51 +0300

    Add isTitle param to Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import classes from "./Header.module.scss";
 
-const Header = () => {
+interface HeaderProps {
+    isTitle: any
+}
+
+const Header = ({isTitle}: HeaderProps) => {
     return (
         <div className={classes.header}>
             <div className={classes.header__container}>
                 <div className={classes.image} style={{background: "none"}}/>
-                <div className={classes.title}>
+                <div className={isTitle ? classes.title : classes.title + " " + classes.noneTitle}>
                     <h1>Тренажер "Поле зрения"</h1>
                 </div>
                 <div className={classes.image}/>
