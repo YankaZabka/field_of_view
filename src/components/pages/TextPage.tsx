53cb36f52a3e9818104dd0342ace5d0e26d4f4d9
YankaZabka <yanka.zabka@gmail.com>
2021-11-22 15:30:45 +0300

    TextPage realisation

--- src/components/pages/TextPage.tsx
+++ src/components/pages/TextPage.tsx
@@ -1,18 +1,71 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import classes from "./TextPage.module.scss"
 import {IOptions} from "../../interfaces/options";
+import {library} from "../../data/library/library";
 
 interface TextPageProps {
     options: IOptions | undefined
+
     onEnd(): void
 }
 
+const _ = require("lodash")
+
 const TextPage = ({options, onEnd}: TextPageProps) => {
+    const [words] = useState(_.sampleSize(library.filter(word => word.length === options?.lettersInWords), options?.numberOfWords))
+    const [currentWordIndex, setCurrentWordIndex] = useState(0)
+    const [separatingIndex, setSeparatingIndex] = useState<number>()
+    const [distance, setDistance] = useState(options!.startingDistance)
+
+    function getIndex(str: string): number {
+
+        if (str.length % 2 === 0) {
+            return str.length / 2
+        } else if (Math.random() < 0.5) {
+            return Math.floor(str.length / 2)
+        } else {
+            return Math.ceil(str.length / 2)
+        }
+
+    }
+
+    useEffect(() => {
+        if (currentWordIndex === 0) {
+            setSeparatingIndex(getIndex(words[currentWordIndex]))
+        }
+
+        setTimeout(() => {
+
+            if (currentWordIndex === words.length - 1) {
+                onEnd()
+            } else {
+                setSeparatingIndex(getIndex(words[currentWordIndex]))
+                setDistance(prev => prev + options!.increasingDistance)
+                setCurrentWordIndex(prev => prev + 1)
+            }
+        }, options!.speed * 1000)
+    }, [currentWordIndex])
+
     return (
+
         <div className={classes.page}>
-            <div className={classes.leftPart}>разв</div>
+            <div
+                className={classes.leftPart}
+                style={{marginRight: distance}}
+            >
+                {
+                    words[currentWordIndex].slice(0, separatingIndex)
+                }
+            </div>
             <div className={classes.tilde}/>
-            <div className={classes.rightPart}>итие</div>
+            <div
+                className={classes.rightPart}
+                style={{marginLeft: distance}}
+            >
+                {
+                    words[currentWordIndex].slice(separatingIndex)
+                }
+            </div>
         </div>
     );
 };
