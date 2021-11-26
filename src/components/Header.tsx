ec70b5de0bb82a050df8ed8c03a37dec64141326
YankaZabka <yanka.zabka@gmail.com>
2021-11-26 15:35:15 +0300

    Add LanguageSelector component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,16 +1,22 @@
 import React from 'react';
 import classes from "./Header.module.scss";
 import {FormattedMessage} from "react-intl";
+import LanguageSelector from "./controls/shared/LanguageSelector";
 
 interface HeaderProps {
     isTitle: boolean
+    currentLocale: string
+    onLocaleChange(value: string): void
 }
 
-const Header = ({isTitle}: HeaderProps) => {
+const Header = ({isTitle, currentLocale, onLocaleChange}: HeaderProps) => {
     return (
         <div className={classes.header}>
             <div className={classes.header__container}>
-                <div className={classes.image} style={{background: "none"}}/>
+                <LanguageSelector
+                    currentLocale={currentLocale}
+                    onLocaleChange={(value) => onLocaleChange(value)}
+                />
                 <div className={isTitle ? classes.title : classes.title + " " + classes.noneTitle}>
                     <h1>
                         <FormattedMessage id="title"/>
