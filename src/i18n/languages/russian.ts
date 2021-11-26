f9d4ece9adddb28a9fa9cfd8458b6cb4c5091c00
YankaZabka <yanka.zabka@gmail.com>
2021-11-26 15:12:16 +0300

    Add languages data

---
+++ src/i18n/languages/russian.ts
@@ -0,0 +1,14 @@
+export const RUSSIAN = {
+    title: `Тренажер "Поле зрения"`,
+    number_of_words:
+        'Сколько слов',
+    starting_distance:
+        'Стартовое расстояние',
+    letters_in_words: 'Сколько букв в словах',
+    increasing_distance: 'Увеличение расстояния',
+    speed: 'Скорость',
+    speed_value: "сек.",
+    start_button: "СТАРТ",
+    finish: "Отличная работа!",
+    languages: "Языки",
+}
---
+++ src/i18n/locales.ts
@@ -0,0 +1,4 @@
+export const LOCALE = {
+    ENGLISH: 'en-US',
+    RUSSIAN: 'ru-RU',
+};
