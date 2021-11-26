f348ca1e94c60fe2a3576ac40a3bbdc9f8d96b99
YankaZabka <yanka.zabka@gmail.com>
2021-11-26 15:12:16 +0300

    Add languages data

---
+++ src/i18n/languages/english.ts
@@ -0,0 +1,14 @@
+export const ENGLISH = {
+    title: `"Field of view" simulator`,
+    number_of_words:
+        'How many words',
+    starting_distance:
+        'Starting distance',
+    letters_in_words: 'How many letters in words',
+    increasing_distance: 'Increasing distance',
+    speed: 'Speed',
+    speed_value: "sec.",
+    start_button: "START",
+    finish: "Great job!",
+    languages: "Languages",
+}
---
+++ src/i18n/messages.ts
@@ -0,0 +1,8 @@
+import {LOCALE} from './locales'
+import {ENGLISH} from "./languages/english";
+import {RUSSIAN} from "./languages/russian";
+
+export const messages = {
+    [LOCALE.ENGLISH]: ENGLISH,
+    [LOCALE.RUSSIAN]: RUSSIAN,
+}
